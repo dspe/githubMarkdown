cdba392c61ad27a1459b30b931678b7511418221
dspe <vincent.royol@gmail.com>
2013-05-13 15:18:07 +0200

    Added option to command line

--- githubMarkdown.rb
+++ githubMarkdown.rb
@@ -19,40 +19,50 @@ require 'rubygems'
 require 'redcarpet'
 require 'pathname'
 require 'pygments.rb'
+require 'optparse'
+
+$options = {}
+
+optparse = OptionParser.new do|opts|
+  opts.banner = "Usage: githubMarkdown.rb [options] input.md output.html"
+  
+  $options[:toc] = false
+  opts.on( '-t', '--toc', 'Add a table of contents - Replace ::generate_toc') do
+    $options[:toc] = true
+  end
+  
+  # This displays the help screen, all programs are
+  # assumed to have this option.
+  opts.on( '-h', '--help', 'Display this screen') do
+    puts opts
+    exit
+  end
+end
 
 class HTMLwithPygments < Redcarpet::Render::XHTML
-	def doc_header()
-    ghf_css_path = File.join Dir.pwd,
-                              'githubMarkdown.css'
-	#	puts Pygments.styles()
-			# monokai
-			# manni
-			# perldoc
-			# borland
-			# colorful
-			# default
-			# murphy
-			# vs
-			# trac
-			# tango
-			# fruity
-			# autumn
-			# bw
-			# emacs
-			# vim
-			# pastie
-			# friendly
-			# native
-	# 	'<style>' + Pygments.css('.highlight',:style => 'vs') + '</style>'
+  @@number = -1
+  
+  def doc_header()
+    ghf_css_path = File.join Dir.pwd, 'githubMarkdown.css'
+	  #	puts Pygments.styles()
+			# monokai manni perldoc borland colorful default murphy vs trac tango fruity autumn bw emacs vim pastie friendly native
+			#	'<style>' + Pygments.css('.highlight',:style => 'vs') + '</style>'
     if UNSTYLED 
       '<div class="md"><article>'
     else
       '<style>' + File.read(ghf_css_path) + '</style><div class="md"><article>'
     end
 	end
+	
+	def header( text, header_level )
+	  @@number += 1
+	  "<h#{header_level} id=\"toc_#{@@number}\">#{text}</h#{header_level}>"
+  end
+  
   def doc_footer
     '</article></div>'
   end
+  
 	def block_code(code, language)
 		Pygments.highlight(code, :lexer => language, :options => {:encoding => 'utf-8'})
 	end
@@ -70,9 +80,38 @@ def fromMarkdown(text)
 		:superscript => true,
 		:hard_wrap => true,
 		:tables => true,
-		:xhtml => true)
-	markdown.render(text)
+		:xhtml => true,
+		:with_toc_data => true
+	)
+	toc = Redcarpet::Markdown.new( Redcarpet::Render::HTML_TOC )
+	
+	render_html = markdown.render( text )
+	if $options[:toc]
+	  render_toc  = toc.render( text )
+		full = render_html.sub( "::generate_toc", render_toc )
+		
+		return full
+	else
+	  return render_html
+  end
 end
 
-UNSTYLED = (ARGV.first == '--unstyled')
-puts fromMarkdown(STDIN.read)
+optparse.parse!
+#UNSTYLED = (ARGV.first == '--unstyled')
+UNSTYLED = false
+
+first, second = ARGV
+
+begin
+  inputFile   = File.open( first )
+  outputFile  = File.open( second, 'w' )
+  
+  outputFile.write( fromMarkdown( inputFile.read() ) )
+rescue IOError => e
+  puts "Could not open files"
+  puts e.message
+ensure
+  inputFile.close unless inputFile == nil
+  outputFile.close unless outputFile == nil
+end
+
